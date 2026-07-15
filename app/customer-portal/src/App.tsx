3a2ddc51332530c04628e06be8c8114316024e3b
satishgowda28 <satishgowda28@gmail.com>
2026-07-15 09:05:45 +0530

    feat: a feature added

--- app/customer-portal/src/App.tsx
+++ app/customer-portal/src/App.tsx
@@ -16,7 +16,6 @@ function App() {
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
-          <p>Hello World</p>
         </div>
       </section>
 
